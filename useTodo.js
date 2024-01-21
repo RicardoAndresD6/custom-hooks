9f85c9c0c34f9b3105726ae1fa2faac5e11fbcf7
Ric <ricardoandresd6@gmail.com>
2024-01-21 15:39:28 -0300

    custom hooks

---
+++ useTodo.js
@@ -0,0 +1,54 @@
+import { useEffect, useReducer } from "react";
+import { TodoReducer } from "../08-reducer/intro-reducer/TodoReducer";
+
+const init = () => {
+    return JSON.parse(localStorage.getItem('todos')) || [];
+};
+
+export const useTodo = () => {
+    
+    const [todos, dispatch] = useReducer(TodoReducer, [], init);
+
+    useEffect(() => {
+
+        localStorage.setItem('todos', JSON.stringify(todos) || [] );
+
+    }, [todos]);
+    
+
+    const handleNewTodo = (todo) => {
+        const action = {
+            type: 'add',
+            payload: todo
+        };
+
+        dispatch(action);
+    };
+
+    const handleDelete = (id) => {
+        dispatch({
+            type: 'delete',
+            payload: id
+        });
+    };
+
+    const handleToggleTodo = (id) => {
+        dispatch({
+            type: 'toggle',
+            payload: id
+        });
+    };
+
+    const todosCount = todos.length;
+
+    const pendingTodosCount = todos.filter( todo => !todo.done ).length;
+
+    return{
+        todos,
+        handleNewTodo,
+        handleDelete,
+        handleToggleTodo,
+        todosCount,
+        pendingTodosCount
+    };
+}
